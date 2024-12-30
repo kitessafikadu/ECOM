21ec480e4f14d2c0f50eb5a303e27c54db5029fd
Kitessa Fikadu <kitessafikadu@gmail.com>
2024-12-30 23:43:17 +0300

    added star rating

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,8 +1,10 @@
+import StarRating from "./StarRating";
 const ProductCard = ({ name, price }) => {
   return (
     <div className="p-4 border rounded shadow-sm">
       <h2 className="text-lg font-bold">{name}</h2>
       <p className="text-gray-700">${price}</p>
+      <StarRating />
     </div>
   );
 };
